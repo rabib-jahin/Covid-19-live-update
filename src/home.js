d1bb1062d407b20892de43d857ba15c6199a5af0
rabib-jahin <1805003@ugrad.cse.buet.ac.bd>
2020-06-13 17:22:18 +0600

    good

--- src/home.js
+++ src/home.js
@@ -19,16 +19,27 @@ return <Router> <div>
 
 <h1>Covid 19 Live Update</h1>
 </div>
-<div class="left">
-
-<ul>
-<li ><Link to="/"  ><a class="link">Home</a></Link></li>
-  <li ><Link to="/App"><a class="link">Covid Info(World wide)</a></Link></li>
-  <li ><Link to="/bd"><a class="link">Bangladesh Info</a></Link></li>
-
-</ul>
-</div>
-
+<div id="nav"></div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark " >
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><Link to="/">Home</Link> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><Link to="/App" >Covid Info(World wide)</Link></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><Link to="/bd"> Bangladesh Info</Link></a>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
 
 </div>
 <Switch>
